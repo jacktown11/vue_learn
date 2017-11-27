10f0228ca127cfbbcfc2395bfe98009d690876b9
jacktown11 <jikai_tang@163.com>
2017-11-27 18:44:53 +0800

    Form input bindings

--- test.js
+++ test.js
@@ -1,51 +1,27 @@
 var example1 = new Vue({
 	el: '#example-1',
 	data: {
-		counter: 0
-	}
-});
-
-var example2 = new Vue({
-	el: '#example-2',
-	data: {
-		name: 'vue.js'
+		message: '',
+		checked: false,
+		checkedNames: [],
+		radio: '',
+		selected: '',
+		selectedM: [],
+		options: [
+			{text: 'A', value: 'A'},
+			{text: 'B', value: 'B'},
+			{text: 'C', value: 'C'}
+		],
+		selected3: '',
+		a: 'good',
+		b: 'bad',
+		toggle: '',
+		radiox: '',
+		select1645: []
 	},
 	methods: {
-		greet: function(event){
-			console.log('Hello, this is ' + this.name);
-
-			//if a native dom event
-			if(event){
-				console.log('this is from tag ' + event.target.tagName)
-			}
+		report: function(){
+			console.log(typeof this.checked);
 		}
 	}
-});
-
-var example3 = new Vue({
-	el: '#example-3',
-	data: {
-
-	},
-	methods: {
-		say: function(message){
-			console.log(message);
-		}
-	}
-});
-
-var example4 = new Vue({
-	el: '#example-4',
-	data: {
-
-	},
-	methods: {
-		warn: function(message,event){
-			if(event){
-				event.preventDefault();
-			}
-			console.log(message);
-		}
-	}
-});
-
+})
